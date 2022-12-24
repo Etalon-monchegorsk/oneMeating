ed0dce54249cceba9130291947028e5e8642b27b
Etalon-monchegorsk <bitnit633@gmail.com>
2022-12-24 14:53:48 +0300

    Первая зарузка

---
+++ src/Gleb.js
@@ -0,0 +1,12 @@
+import './Gleb.css';
+import picture from './logo.svg';
+
+function Gleb() {
+    return (
+        <div className="Gleb">
+            <img src={picture}></img>
+        </div>
+    );
+}
+
+export default Gleb;
---
+++ src/Hgtt.js
@@ -0,0 +1,11 @@
+function Hgtt() {
+    return (
+        <div className="Hgtt">
+            <h1>Hello earth</h1>
+            <p>Gdfhsdfhvcdfr</p>
+            <a href="vk.com">vk</a>
+        </div>
+    );
+}
+
+export default Hgtt;
